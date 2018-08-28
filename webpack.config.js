e6081e9e1f25caa7520678d26c2e67f508fde6ef
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2018-08-28 17:09:22 +0100

    ch(setup): setup empty react project
    - install and configure webpack
    
    - install and configure babel
    
    - install and configure react
    
    [Finishes #159989005]

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+ const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['env', 'react'] },
+        resolve: { extensions: ['.js', '.jsx'] },
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 5000,
+          },
+        },
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[hash].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'client/public'),
+    historyApiFallback: true,
+  },
+  plugins: [htmlWebpackPlugin],
+};
