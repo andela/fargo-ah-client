b7dc343f8e7763e589c722e45b08bc9bdd809ecb
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2018-08-28 17:09:22 +0100

    ch(setup): setup empty react project
    - install and configure webpack
    
    - install and configure babel
    
    - install and configure react
    
    [Finishes #159989005]

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => <div>Hello Authors Haven!</div>;
+
+ReactDOM.render(<Index />, document.getElementById('index'));
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 5000,
+          },
+        },
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[hash].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    htmlWebpackPlugin,
+  ],
+};
