664c63e1ef62efc4b06dee823ce5114b1e84f16f
Olumide Ogundele <olumide.ogundele@andela.com>
2018-08-29 16:25:15 +0100

    ch(redux): Setup project for redux
    	- install redux, react-redux, redux-thunk packages
    	- create folders for redux and root file
    
    [Delivers #159989006]

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+<<<<<<< HEAD
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -8,5 +9,11 @@ import rootReducer from '../reducers';
 const store = process.env.NODE_ENV === 'development'
   ? createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(logger, reduxThunk)))
   : createStore(rootReducer, {}, applyMiddleware(reduxThunk));
+=======
+
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
+>>>>>>> ch(redux): Setup project for redux
 
 export default store;
