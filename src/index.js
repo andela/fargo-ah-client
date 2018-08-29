c132deba48bcd1c26d1e7288631a63188bb0c5be
maureen <oki.maureen.eloho@gmail.com>
2018-08-29 23:28:55 +0100

    ch(router): Setup react router
    - install react-router and react-router-dom
    - create folder for router and configure react router in index file
    - create view and component folder
    - create home and login component in view folder
    - route Home component to '/' path
    - route Login component to '/login' path
    - add github pull request template
    
    [Finishes #159989009]

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import Routes from './router';
 import './styles/index.scss';
 
+
+
+
 ReactDOM.render(<Routes />, document.getElementById('index'));
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+<<<<<<< HEAD
 import Home from '../views/Home';
 import Login from '../views/Login';
 
 const Routes = () => (
+=======
+import Home from 'views/Home';
+import Login from 'views/Login';
+
+
+const Routes = () => ((
+>>>>>>> ch(router): Setup react router
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
+<<<<<<< HEAD
 );
+=======
+));
+>>>>>>> ch(router): Setup react router
 export default Routes;
