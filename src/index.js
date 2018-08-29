744314e312fcb3f8bf058b5678d81d6e9c1b9ff2
maureen <oki.maureen.eloho@gmail.com>
2018-08-29 23:28:55 +0100

    ch(router): Setup react router
    - install react-router and react-router-dom
    - create folder for router and configure react router in index file
    - create view and component folder
    - create home and login component in view folder
    - route Home component to '/' path
    - route Login component to '/login' path
    - add github pull request template
    
    [Finishes #159989009]

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -11,3 +12,10 @@ const App = () => (
   </Provider>
 );
 render(<App />, document.getElementById('index'));
+=======
+import ReactDOM from 'react-dom';
+import Routes from './router';
+import './scss/style.scss';
+
+ReactDOM.render(<Routes />, document.getElementById('index'));
+>>>>>>> ch(router): Setup react router
