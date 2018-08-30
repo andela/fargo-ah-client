48b41325bdd4f858e0ed55cbb7c00ee22a42982c
Onengiye Richard <klevamane@gmail.com>
2018-08-30 10:19:36 +0100

    ch(sass): setup sass processing on client
    - add sass-loader to webpack
    - configure webpack to test for scss
    
    [Finishes #159989010]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './router';
+import './scss/style.scss';
+
 
 ReactDOM.render(<Routes />, document.getElementById('index'));
