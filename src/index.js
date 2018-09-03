f4a8bdccba7ec8e6bd82130d48fdfb79f66b58c5
Veeqtor <nwokeochavictor@gmail.com>
2018-09-03 21:09:52 +0100

    ch(styling): Create scss file for colours
    - Add stylelint
    - Create scss color file
    - Configure hound for stylelint
    
    [Delivers #160238406]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './router';
-import './scss/style.scss';
+import './styles/index.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('index'));
