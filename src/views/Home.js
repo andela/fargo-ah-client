14ab481dfd10c669c32d8f350c1243fc83e06278
Kalu Nwanna <nwannauk@gmail.com>
2018-09-04 12:14:27 +0100

    ft(avatar): create user avatar component
    - write unit tests
    - create avatar with semantic-ui-react
    - create dropdown to display options
    - add favicon using app logo
    
    [Delivers #160247659]

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Grid, Card, Button } from 'semantic-ui-react';
 import HeaderCard from '../components/HeaderCard';
-import { articles } from '../tests/__mocks__/mockData';
+import Avatar from '../components/Avatar';
+import { articles, user } from '../tests/__mocks__/mockData';
 
 const Home = () => (
   <div>
     <Button text="Engage" />
+    <Avatar user={user} />
     <Grid id="header-card" stackable>
       <HeaderCard articles={articles.articles} />
     </Grid>
@@ -13,6 +15,7 @@ const Home = () => (
       {/* Replace the articles prop with the response from the get articles request call  */}
       <HeaderCard articles={articles} />
     </Card.Group>
+
   </div>
 );
 
