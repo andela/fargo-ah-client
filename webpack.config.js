f36ab0944b8866e9620f69d6eb91c88d97ff7859
Veeqtor <nwokeochavictor@gmail.com>
2018-09-04 09:27:06 +0100

    ft(HeaderCard): Create the header card components
    - Write unit tests
    - Add semantic-ui CDN
    - Add semanetic-ui-react
    - Create the header card component
    
    [Delivers #160238582]

--- webpack.config.js
+++ webpack.config.js
@@ -9,24 +9,54 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
+<<<<<<< HEAD
         options: { presets: ['env', 'react'] },
         resolve: { extensions: ['.js', '.jsx'] },
       },
       {
         test: /\.s?css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+=======
+        options: { presets: ['env', 'react', 'stage-2'] },
+        resolve: { extensions: ['.js', '.jsx'] },
+      },
+      {
+<<<<<<< HEAD
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+=======
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=5000&mimetype=application/font-woff',
+>>>>>>> ft(HeaderCard): Create the header card components
+>>>>>>> ft(HeaderCard): Create the header card components
       },
       {
         test: /\.(jpg|png)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 5000,
-          },
-        },
+        loader: 'url-loader?limit=5000',
       },
       {
         test: /\.(jpg|png|svg)$/,
