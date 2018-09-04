457360f1b5eb9efc3e16b09f32ce3a00c8c7c396
Veeqtor <nwokeochavictor@gmail.com>
2018-09-04 09:27:06 +0100

    ft(HeaderCard): Create the header card components
    - Write unit tests
    - Add semantic-ui CDN
    - Add semanetic-ui-react
    - Create the header card component
    
    [Delivers #160238582]

---
+++ src/components/HeaderCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import '../styles/headCard.scss';
+
+const HeaderCard = ({ articles }) => {
+  const latestArticles = articles.articles.slice(-3);
+  return latestArticles.map(article => (
+    <Card key={article.slug}>
+      <Image src={article.imageUrl} />
+      <Card.Content>
+        <Card.Header>Tech</Card.Header>
+        <Card.Description>
+          <Link to={`/articles/${article.slug}`}>{article.title}</Link>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Link to={`/profiles/${article.author.username}`}>
+          <Image className="avatar" src={article.author.image} />
+          {article.author.username}
+        </Link>
+        <span className="date">{moment(article.createdAt).fromNow()}</span>
+      </Card.Content>
+    </Card>
+  ));
+};
+HeaderCard.propTypes = {
+  articles: PropTypes.shape().isRequired,
+};
+
+export default HeaderCard;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
+import HeaderCard from '../components/HeaderCard';
+import articles from '../tests/__mocks__/mockData';
 
-const Home = () => <h1>FARGO REACT APP - TEST HOME VIEW</h1>;
+const Home = () => (
+  <div>
+    <h1>FARGO REACT APP - TEST HOME VIEW</h1>
+    <Card.Group centered>
+      {/* Replace the articles prop with the response from the get articles request call  */}
+      <HeaderCard articles={articles} />
+    </Card.Group>
+  </div>
+);
 
 export default Home;
