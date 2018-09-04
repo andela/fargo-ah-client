1bdfae3a9d2dcfd5ff795035e7cfc527db749e26
Veeqtor <nwokeochavictor@gmail.com>
2018-09-04 09:27:06 +0100

    ft(HeaderCard): Create the header card components
    - Write unit tests
    - Add semantic-ui CDN
    - Add semanetic-ui-react
    - Create the header card component
    
    [Delivers #160238582]

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Routes from './router';
-import './styles/index.scss';
 
 const App = () => (
   <Provider store={store}>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
+import HeaderCard from '../components/HeaderCard';
+import articles from '../tests/__mocks__/mockData';
 
-const Home = () => <h1>FARGO REACT APP - TEST HOME VIEW</h1>;
+const Home = () => (
+  <div>
+    <h1>FARGO REACT APP - TEST HOME VIEW</h1>
+
+    {/* Replace the articles prop with the response from the get articles request call  */}
+    <Card.Group id="header-card" centered>
+      <HeaderCard articles={articles} />
+    </Card.Group>
+  </div>
+);
 
 export default Home;
