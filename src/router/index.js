3e16d2a6cb6c5239aa77163f3ec1f356ead8ba6e
Maureen Oki <maureenoki@Maureens-MacBook-Pro.local>
2018-09-06 15:49:47 +0100

    renamed button.js to Button.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,101 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> ft(component): create button component
 import Home from '../views/Home';
 import Login from '../views/Login';
 
-<<<<<<< HEAD
 const Routes = () => (
-=======
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-const Routes = () => ((
-=======
-const Routes = () => (
-=======
-import Home from 'views/Home';
-import Login from 'views/Login';
-
-
-const Routes = () => ((
->>>>>>> ch(router): Setup react router
->>>>>>> ch(router): Setup react router
->>>>>>> ch(router): Setup react router
-<<<<<<< HEAD
-=======
-=======
-=======
-=======
-=======
->>>>>>> ft(component): create button component
-import Home from '../views/Home';
-import Login from '../views/Login';
-import '../styles/index.scss';
-
-const Routes = () => (
-<<<<<<< HEAD
->>>>>>> ch(testing): Setup testing environment
->>>>>>> ch(testing): Setup testing environment
->>>>>>> ch(testing): Setup testing environment
-=======
->>>>>>> ft(component): create button component
->>>>>>> ft(component): create button component
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> ft(component): create button component
 );
-=======
-<<<<<<< HEAD
-));
-=======
-<<<<<<< HEAD
-);
-=======
-));
->>>>>>> ch(router): Setup react router
->>>>>>> ch(router): Setup react router
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-=======
-=======
->>>>>>> ft(component): create button component
-);
-
-<<<<<<< HEAD
-const Routes = () => ((
-    <BrowserRouter>
-    <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-    </Switch>
-    </BrowserRouter>
-  ));
-  
-  export default Routes;
-  
->>>>>>> ch(testing): Setup testing environment
->>>>>>> ft(component): create button component
->>>>>>> ch(router): Setup react router
-=======
->>>>>>> ft(component): create button component
 export default Routes;
