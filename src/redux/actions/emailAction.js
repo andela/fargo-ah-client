213be7a4c3a86ac9cba121766ff00a5bed1dc8ee
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2018-09-14 15:54:58 +0100

    feat(social-login/signup): implement social authentication
    - enable social authentication
    - add unit test and integration test
    
    [Delivers #159989016]

--- src/redux/actions/emailAction.js
+++ src/redux/actions/emailAction.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import process from '../../../api';
 
 export default token => () => axios(`${process.env.BACKEND_URL}/api/users/verify/${token}`);
 export const resendEmail = email => () => axios.post(`${process.env.BACKEND_URL}/api/users/reverify`, { user: { email } });
