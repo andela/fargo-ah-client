34074039cd0a6920618c1aba9da21aeea620af07
Maureen Oki <maureenoki@Maureens-MacBook-Pro.local>
2018-09-19 12:02:17 +0100

    ft(view): create article view
    - install semantic ui react
    - add create article component
    - add tinyMCE wysiwyg editor
    
    [Delivers #159989018]

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import resetReducer from './reset';
 import currentUser from './userReducer';
 import loadedCategories from './loadCategories';
 import loadedArticles from './loadedArticles';
+import createArticle from './articleReducers';
 
 const rootReducer = combineReducers({
   resetReducer,
   loadedCategories,
   currentUser,
   loadedArticles,
+  createArticle,
 });
 
 export default rootReducer;
--- src/views/CreateArticle.js
+++ src/views/CreateArticle.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import {
-  Divider, Grid, Checkbox, Form, TextArea, Input,
+  Divider, Grid, Checkbox, Form, Input,
 } from 'semantic-ui-react';
 import { Editor } from '@tinymce/tinymce-react';
-
-import HeaderComponent from '../components/Header/HeaderComponent';
-import FooterSlim from '../components/FooterSlim';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Button from '../components/Button';
-import { user } from '../tests/__mocks__/mockData';
+import articleAction from '../redux/actions/articleAction';
 import '../styles/index.scss';
 
 import 'tinymce/themes/modern/theme';
@@ -31,14 +31,10 @@ import 'tinymce/plugins/fullscreen';
 import 'tinymce/plugins/insertdatetime';
 import 'tinymce/plugins/media';
 import 'tinymce/plugins/contextmenu';
-import 'tinymce/plugins/codesample';
+// import 'tinymce/plugins/codesample';
 import 'tinymce/plugins/imagetools';
 import 'tinymce/plugins/textcolor';
 import 'tinymce/plugins/table';
-
-const submit = (e) => {
-  console.log('@here', e.target.getContent());
-};
 
 const titleConfig = {
   menubar: false,
@@ -56,7 +52,7 @@ const bodyConfig = {
   theme: 'inlite',
   plugins: [
     'autolink',
-    'codesample',
+    // 'codesample',
     'contextmenu',
     'link',
     'lists',
@@ -72,77 +68,131 @@ const bodyConfig = {
   insert_toolbar: 'quickimage quicktable',
   selection_toolbar: 'bold italic | h2 h3 | blockquote quicklink quickimage | forecolor backcolor | alignleft aligncenter alignright alignfull | link unlink | numlist bullist outdent indent',
   contextmenu: 'inserttable | cell row column deletetable',
-  // powerpaste_word_import: 'clean',
-  // powerpaste_html_import: 'clean',
+  powerpaste_word_import: 'clean',
+  powerpaste_html_import: 'clean',
 };
 
 
-const CreateArticle = () => (
-  <div>
-    {/* <HeaderComponent text="home" user={user} pathname="/" /> */}
-    <Grid container centered stackable className="layout">
-      <Form onSubmit={submit}>
-        <Grid.Row>
-          <Grid.Column column={12}>
-            <Grid.Row>
-              <Editor onChange={submit}
-                initialValue=""
-                init={titleConfig}  />
-              <Divider />
-              <span className="image-button">
-                <label htmlFor="image">
+export class CreateArticle extends Component {
+  state= {
+    titleEditor: '',
+    bodyEditor: '',
+    tagList: '',
+    categorylist: '',
+    imageData: '',
+    isPaidFor: '',
+    price: '',
+  }
+
+  handleTitleEditor = (editor) => {
+    this.setState({ titleEditor: editor.getContent() });
+    return true;
+  }
+
+  handleBodyEditor = (editor) => {
+    this.setState({ bodyEditor: editor.getContent() });
+    return true;
+  }
+
+     handleCreateArticle = (e) => {
+       e.preventDefault();
+       this.props.articleAction(this.state, this.props.history);
+     }
+
+     render() {
+       const { titleEditor } = this.state;
+       const { bodyEditor } = this.state;
+       console.log(bodyEditor, 'render part');
+       return (
+         <div>
+           {/* <HeaderComponent text="home" user={user} pathname="/" /> */}
+           <Grid container centered stackable className="layout">
+             <Form onSubmit={this.handleCreateArticle}>
+               <Grid.Row>
+                 <Grid.Column column={12}>
+                   <Grid.Row>
+                     <Editor
+                       onChange={this.handleTitleEditor}
+                       initialValue={titleEditor}
+                       init={titleConfig}
+                    // value={titleEditor}
+                       ref={this.setEditorRef}
+                     />
+                     <Divider />
+                     <span className="image-button">
+                       <label htmlFor="image">
                     Add Image
-                  <input placeholder="Add Image" id="image" accept="image/*" type="file" className="input" />
-                </label>
-              </span>
-              <Editor
-                onChange={submit}
-                initialValue=""
-                className="text-body"
-                init={bodyConfig}
-                autoHeight
-                placeholder="Share your story with the world"
-                style={
+                         <input
+                           placeholder="Add Image"
+                           id="image"
+                           accept="image/*"
+                           type="file"
+                           className="input"
+                         />
+                       </label>
+                     </span>
+                     <Editor
+                       onChange={this.handleBodyEditor}
+                       initialValue={bodyEditor}
+                       className="text-body"
+                       init={bodyConfig}
+                       placeholder="Share your story with the world"
+                       style={
                 {
                   minHeight: 100,
-                  Height: 100,
                 }}
-                rows="0"
-              />
-            </Grid.Row>
-            <Grid.Row>
-              <div className="header">
-                <p>Let the world know where to look, Tag your story</p>
-                <Input fluid="true" />
-              </div>
-            </Grid.Row>
-            <Grid stackable>
-              <Grid.Row columns={3} className="ce">
-                <Grid.Column>
-                  <Checkbox label="Free for all" />
-                  <Checkbox label="Premuim" />
-                </Grid.Column>
-                <Grid.Column>
-                  <p>Min: $0.28    Max: $5.53</p>
-                  <input type="number" className="text-body" autoHeight id="price" placeholder="Write Price Here" />
-                </Grid.Column>
-                <Grid.Column>
-                  <div className="options">
-                    <div>
-                      <Button size="tiny" text="Save as Draft" />
-                    </div>
-                    <div>
-                      <Button type="submit" size="tiny" text="Publish" />
-                    </div>
-                  </div>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-        </Grid.Row>
-      </Form>
-    </Grid>
-    {/* <FooterSlim /> */}
-  </div>
-);
-export default CreateArticle;
+                       rows="0"
+                     />
+                   </Grid.Row>
+                   <Grid.Row>
+                     <div className="header">
+                       <p>Let the world know where to look, Tag your story</p>
+                       <Input fluid="true" />
+                     </div>
+                   </Grid.Row>
+                   <Grid stackable>
+                     <Grid.Row columns={3} className="ce">
+                       <Grid.Column>
+                         <Checkbox label="Free for all" />
+                         <Checkbox label="Premuim" />
+                       </Grid.Column>
+                       <Grid.Column>
+                         <p>Min: $0.28    Max: $5.53</p>
+                         <input type="number" className="text-body" id="price" placeholder="Write Price Here" />
+                       </Grid.Column>
+                       <Grid.Column>
+                         <div className="options">
+                           <div>
+                             <Button size="tiny" text="Save as Draft" />
+                           </div>
+                           <div>
+                             <Button type="submit" size="tiny" text="Publish" />
+                           </div>
+                         </div>
+                       </Grid.Column>
+                     </Grid.Row>
+                   </Grid>
+                 </Grid.Column>
+               </Grid.Row>
+             </Form>
+           </Grid>
+           {/* <FooterSlim /> */}
+         </div>
+       );
+     }
+}
+const mapDispatchToProps = dispatch => bindActionCreators({
+  articleAction,
+}, dispatch);
+
+const mapStateToProps = state => ({
+  articleDetail: state.createdArticle,
+});
+
+CreateArticle.propTypes = {
+  articleDetail: PropTypes.objectOf(PropTypes.object).isRequired,
+  articleAction: PropTypes.func.isRequired,
+  history: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateArticle);
